bb919111e0b518726297ffcce58297148b4de273
pUrGe12 <helloitsme0218@gmail.com>
2025-10-21 22:27:23 +0530

    name change

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="pyba",
+    name="py-browser-automation",
     version="0.1.0",
     author="pUrGe12",
     author_email="achintya.jai@owasp.org",
