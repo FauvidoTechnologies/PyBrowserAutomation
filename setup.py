a4d1768b0b0be59cb634cc681e10c18595c8a960
pUrGe12 <helloitsme0218@gmail.com>
2025-10-23 16:32:57 +0530

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="py-browser-automation",
-    version="0.1.4",
+    version="0.1.5",
     author="pUrGe12",
     author_email="achintya.jai@owasp.org",
     url="https://github.com/FauvidoTechnologies/PyBrowserAutomation",
