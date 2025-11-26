c7dabba075258c1cc2c7d22c5560cbbb86debc50
pUrGe12 <helloitsme0218@gmail.com>
2025-11-27 01:35:58 +0530

    added new table for extracted output collection

--- pyba/database/models.py
+++ pyba/database/models.py
@@ -24,3 +24,25 @@ class EpisodicMemory(Base):
         return ("EpisodicMemory(session_id: {0}, actions: {1}, page_url: {2})").format(
             self.session_id, self.actions, self.page_url
         )
+
+
+class ExtractedData(Base):
+    """
+    Database for extracted data
+
+    Arguments:
+            - `session_id`: A unique session ID for the run
+            - `extraction_model`: The pydantic BaseModel strcture for reference
+            - `logs`: The actual logs implemented as a growing buffer
+    """
+
+    __tablename__ = "ExtractionData"
+
+    session_id = Column(Text, primary_key=True)
+    extraction_model = Column(Text, nullable=False)
+    logs = Column(Text, nullable=False)
+
+    def __repr__(self):
+        return ("ExtractedData(session_id: {0}, extraction_model: {1}, logs: {2})").format(
+            self.session_id, self.extraction_model, self.logs
+        )
